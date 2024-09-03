e8d9e8ea365e726520ce4bc51024a6f8f204fa8b
Nilesh <nbgosavi5@gmail.com>
2024-09-03 16:48:59 +0530

    fixed vite config typo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://shoppyglobe.nileshgosavi.tech/',
+  base: 'https://shoppyglobe.nileshgosavi.tech/',
 })
